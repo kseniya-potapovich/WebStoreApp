48f092647ecded764d4f91e1875d9b9ff122d2f0
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-09 23:12:02 +0300

    Next commit

---
+++ WedStoreApp.Entities/Seller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WedStoreApp.Entities
+{
+    internal class Seller
+    {
+        public int Id { get; set; }
+
+        public string NameCompany { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public string Reviews { get; set; }
+
+    }
+}
