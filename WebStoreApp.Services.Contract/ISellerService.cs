a32f7bc93223fdc80d7b77dad18633b46d88bc9a
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-23 19:07:39 +0300

    Fifth commit "Add SellerService"

---
+++ WebStoreApp.Services.Contract/ISellerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStoreApp.Dto;
+
+namespace WebStoreApp.Services.Contract
+{
+    public interface ISellerService
+    {
+        public Task<SellerDto> GetById(int id);
+
+        Task<int> Create(SellerDto seller);
+    }
+}
---
+++ WebStoreApp.Services/SellerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStoreApp.DataAccess.Repository;
+using WebStoreApp.DataAccess.Repository.Concracts;
+using WebStoreApp.Dto;
+using WebStoreApp.Services.Contract;
+using WedStoreApp.Entities;
+
+namespace WebStoreApp.Services
+{
+    public class SellerService : ISellerService
+    {
+        public readonly ISellerRepository _sellerRepository;
+
+        public SellerService(ISellerRepository sellerRepository)
+        {
+            _sellerRepository = sellerRepository;
+        }
+
+        public async Task<int> Create(SellerDto seller)
+        {
+            var sellerToAdd = new Seller()
+            {
+                NameCompany = seller.NameCompany,
+                Reviews = seller.Reviews
+            };
+
+            return await _sellerRepository.Create(sellerToAdd);
+        }
+
+        public async Task<SellerDto> GetById(int id)
+        {
+            var seller = await _sellerRepository.GetById(id);
+            var result = new SellerDto()
+            {
+                NameCompany = seller.NameCompany,
+                Reviews = seller.Reviews
+            };
+            return result;
+        }
+    }
+}
